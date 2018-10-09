f283280aee61783745b8743f73a1c66706c6e119
puppy-backend-puller <42998933+puppy-backend-puller@users.noreply.github.com>
2018-10-09 07:26:45 -0700

    update backend ports

--- src/app/ide/backend/managementDB.js
+++ src/app/ide/backend/managementDB.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27217/Puppy');
+mongoose.connect('mongodb://localhost:3032/Puppy');
--- src/app/ide/backend/server.js
+++ src/app/ide/backend/server.js
@@ -1,5 +1,5 @@
 var app = require('./app');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3031;
 
 var server = app.listen(port, function () {
     console.log('Express server listening on port ' + port);
