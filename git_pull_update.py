45f7ad79891afcd35ef8543e1ebd673094eef2d4
Valsamakis Yannis <jvalsam@gmail.com>
2020-11-12 14:17:41 +0200

    update gitigonre, develop script to git pull and build submodules

---
+++ git_pull_update.py
@@ -0,0 +1,29 @@
+##
+# Pull, Update and Build third party repositories of the Blockly Studio
+#
+# Yannis Valsamakis <jvalsam@ics.forth.gr>
+# November 2020
+##
+
+import os
+import sys
+
+
+print(sys.version)
+os.system('git pull')
+
+print('\nPull Submodules of Blockly Studio\n')
+os.system('git submodule update')
+os.system('git submodule foreach git checkout master')
+os.system('git submodule foreach git pull origin master')
+
+print('\nAutomatic IoT User Interfaces:\n')
+AutomaticIoTInterfacesPath = './domain-libs/IoT/iot-interfaces/'
+os.chdir(AutomaticIoTInterfacesPath)
+
+if (not os.path.isdir('./node_modules'):
+    os.system('npm i')
+
+os.system('npm run remote_library')
+
+print('\nBlockly Visual Debugger:\n')
