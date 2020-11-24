fdc4bc8d0eb1574658a60cf0024468fcd5188387
Linaritis Dimitrios <dimilinaritis@gmail.com>
2020-11-24 18:21:57 +0200

    Update sovplelem-view.js

--- src/app/ide/ide-components/SmartObjectVPLEditor/sovpleditor-component/sovplelem-view.js
+++ src/app/ide/ide-components/SmartObjectVPLEditor/sovpleditor-component/sovplelem-view.js
@@ -1058,8 +1058,8 @@ let RenderSmartObjectRegistered = function (
   });
   createGroupsButtonCol.onclick = () => {
     callbacksMap.onCreateSmartGroup({
-      properties: [].concat(soData.editorData.details.properties),
-      mapPropsAlias: Object.assign({}, soData.editorData.details.mapPropsAlias),
+      properties: soData.editorData.details.properties,
+      mapPropsAlias: soData.editorData.details.mapPropsAlias,
       soDataID: soData.editorData.systemID.split("SmartObjectVPLEditor_")[1],
       soName: soData.name,
     });
