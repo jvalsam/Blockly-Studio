4018ef8c211871d50126943809d537dd7bea5e7e
Valsamakis Yannis <jvalsam@gmail.com>
2020-11-29 18:25:08 +0200

    complete develop and fixies to run script for the application domains

--- src/app/application-domain-frameworks/domains-vpl-conf/IoT/execution/run-app.js
+++ src/app/application-domain-frameworks/domains-vpl-conf/IoT/execution/run-app.js
@@ -1,5 +1,17 @@
 
 
-export async function StartApp(appInst) {
-    alert("test");
+export async function StartApplication(execData) {
+    alert("run my application");
+}
+
+export async function StopApplication(execData) {
+    alert("stop my application");
+}
+
+export async function PauseApplication(execData) {
+    alert("pause my application");
+}
+
+export async function ContinueApplication(execData) {
+    alert("continue my application");
 }
---
+++ src/app/ide/components-framework/build-in.components/run-time-system-manager/runtime-environment/runtime-environment-debug.js
@@ -0,0 +1,29 @@
+import { RuntimeEnvironmentScriptsHolder } from "./runtime-environment-scripts-holder.js"
+
+export class RuntimeEnvironmentDebug {
+    constructor(runtimeEnv, envData) {
+        this._runtimeEnv = runtimeEnv;
+        this._envData = envData;
+
+        this._executionScript = RuntimeEnvironmentScriptsHolder
+            .executionDomainFunctions(this._envData.domainType);
+        
+        this.start();
+    }
+
+    start() {
+        this._executionScript.StartApplication();
+    }
+
+    stop() {
+        this._executionScript.StopApplication();
+    }
+
+    pause() {
+        this._executionScript.PauseApplication();
+    }
+
+    continue() {
+        this._executionScript.ContinueApplication();
+    }
+}
