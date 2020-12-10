5328fccf767a6960c3d742f242c3a6ee71ef3e49
Linaritis Dimitrios <dimilinaritis@gmail.com>
2020-12-10 17:10:49 +0200

    updated live preview

--- src/app/application-domain-frameworks/domains-vpl-conf/IoT/execution/run-app.js
+++ src/app/application-domain-frameworks/domains-vpl-conf/IoT/execution/run-app.js
@@ -118,7 +118,7 @@ const StartObserving = function (socket, devicesIDs) {
 const Initialize = function (selector) {
   InitializeClock(selector);
 
-  // InitializeCalendar(selector);
+  InitializeCalendar(selector);
 
   InitializeSmartDevicesContainer(selector);
 };
@@ -255,41 +255,215 @@ const timeDispatch = {
 
 /* Start UI for runtime environment */
 const InitializeClock = function (selector) {
-  let clockDiv = document.createElement("div");
-  clockDiv.id = "runtime-clock";
-  clockDiv.style.fontSize = "larger";
-  selector.appendChild(clockDiv);
+  let outterClockDiv = document.createElement("div");
+  outterClockDiv.style.cssFloat = "right";
+  selector.appendChild(outterClockDiv);
 
-  let date = document.createElement("span");
-  date.id = "runtime-date";
-  date.innerHTML = "Time: ";
-  clockDiv.appendChild(date);
+  let fill = document.createElement("div");
+  fill.classList.add("fill");
+  fill.style.width = "130px";
+  fill.style.height = "130px";
+  outterClockDiv.appendChild(fill);
 
-  let hour = document.createElement("span");
-  hour.id = "runtime-hour";
-  clockDiv.appendChild(hour);
+  let utility_clock = document.createElement("div");
+  utility_clock.id = "utility-clock";
+  utility_clock.classList.add("clock");
+  utility_clock.classList.add("hour-style-text-quarters");
+  utility_clock.classList.add("hour-text-style-large");
+  utility_clock.classList.add("hour-display-style-all");
+  utility_clock.classList.add("minute-style-line");
+  utility_clock.classList.add("minute-display-style-fine");
+  utility_clock.classList.add("minute-text-style-none");
+  utility_clock.classList.add("hand-style-normal");
+  fill.appendChild(utility_clock);
 
-  let calendarButtonSpan = document.createElement("span");
-  clockDiv.appendChild(hour);
+  let centre = document.createElement("div");
+  centre.classList.add("centre");
+  utility_clock.appendChild(centre);
 
-  let calendarFoldButton = document.createElement("button");
-  clockDiv.appendChild(hour);
+  let dynamic = document.createElement("div");
+  dynamic.classList.add("dynamic");
+  centre.appendChild(dynamic);
+
+  let circle_1 = document.createElement("div");
+  circle_1.classList.add("expand");
+  circle_1.classList.add("round");
+  circle_1.classList.add("circle-1");
+  centre.appendChild(circle_1);
+
+  let anchorHour = document.createElement("div");
+  anchorHour.classList.add("anchor");
+  anchorHour.classList.add("hour");
+  centre.appendChild(anchorHour);
+
+  let anchorSub1 = document.createElement("div");
+  anchorSub1.classList.add("element");
+  anchorSub1.classList.add("thin-hand");
+  anchorHour.appendChild(anchorSub1);
+
+  let anchorSub2 = document.createElement("div");
+  anchorSub2.classList.add("element");
+  anchorSub2.classList.add("fat-hand");
+  anchorHour.appendChild(anchorSub2);
+
+  let anchorMinute = document.createElement("div");
+  anchorMinute.classList.add("anchor");
+  anchorMinute.classList.add("minute");
+  centre.appendChild(anchorMinute);
+
+  let anchorMinSub1 = document.createElement("div");
+  anchorMinSub1.classList.add("element");
+  anchorMinSub1.classList.add("thin-hand");
+  anchorMinute.appendChild(anchorMinSub1);
+
+  let anchorMinSub2 = document.createElement("div");
+  anchorMinSub2.classList.add("element");
+  anchorMinSub2.classList.add("fat-hand");
+  anchorMinSub2.classList.add("minute-hand");
+  anchorMinute.appendChild(anchorMinSub2);
+
+  let anchorSecond = document.createElement("div");
+  anchorSecond.classList.add("anchor");
+  anchorSecond.classList.add("second");
+  centre.appendChild(anchorSecond);
+
+  let anchorSecSub1 = document.createElement("div");
+  anchorSecSub1.classList.add("element");
+  anchorSecSub1.classList.add("second-hand");
+  anchorSecSub1.classList.add("second-hand-front");
+  anchorSecond.appendChild(anchorSecSub1);
+
+  let anchorSecSub2 = document.createElement("div");
+  anchorSecSub2.classList.add("element");
+  anchorSecSub2.classList.add("second-hand");
+  anchorSecSub2.classList.add("second-hand-back");
+  anchorSecond.appendChild(anchorSecSub2);
+
+  let circle_2 = document.createElement("div");
+  circle_2.classList.add("expand");
+  circle_2.classList.add("round");
+  circle_2.classList.add("circle-2");
+  centre.appendChild(circle_2);
 };
 
-const RenderClock = function (selector) {
-  // document.getElementById("runtime-date").innerHTML = dayjs().format(
-  //   "dddd, DD MMMM YYYY"
-  // );
+function utilityClock(container) {
+  var dynamic = container.querySelector(".dynamic");
+  var hourElement = container.querySelector(".hour");
+  var minuteElement = container.querySelector(".minute");
+  var secondElement = container.querySelector(".second");
 
-  document.getElementById("runtime-hour").innerHTML = dayjs().format(
-    "hh:mm:ss"
-  );
+  var div = function (className, innerHTML) {
+    var element = document.createElement("div");
+    element.className = className;
+    element.innerHTML = innerHTML || "";
+    return element;
+  };
+
+  var append = function (element) {
+    return {
+      to: function (parent) {
+        parent.appendChild(element);
+        return append(parent);
+      },
+    };
+  };
+
+  var anchor = function (element, rotation) {
+    var anchor = div("anchor");
+    rotate(anchor, rotation);
+    append(element).to(anchor).to(dynamic);
+  };
+
+  var minute = function (n) {
+    var klass = n % 5 == 0 ? "major" : n % 1 == 0 ? "whole" : "part";
+    var line = div("element minute-line " + klass);
+    anchor(line, n);
+    if (n % 5 == 0) {
+      var text = div("anchor minute-text " + klass);
+      var content = div("expand content", (n < 10 ? "0" : "") + n);
+      append(content).to(text);
+      rotate(text, -n);
+      anchor(text, n);
+    }
+  };
+
+  var hour = function (n) {
+    var klass = "hour-item hour-" + n;
+    var line = div("element hour-pill " + klass);
+    anchor(line, n * 5);
+    var text = div("anchor hour-text " + klass);
+    var content = div("expand content", n);
+    append(content).to(text);
+    rotate(text, -n * 5);
+    anchor(text, n * 5);
+    return;
+  };
+
+  var position = function (element, phase, r) {
+    var theta = phase * 2 * Math.PI;
+    element.style.top = (-r * Math.cos(theta)).toFixed(1) + "px";
+    element.style.left = (r * Math.sin(theta)).toFixed(1) + "px";
+  };
+
+  var rotate = function (element, second) {
+    element.style.transform = element.style.webkitTransform =
+      "rotate(" + second * 6 + "deg)";
+  };
+
+  var animate = function () {
+    var now = new Date();
+    var time =
+      now.getHours() * 3600 +
+      now.getMinutes() * 60 +
+      now.getSeconds() * 1 +
+      now.getMilliseconds() / 1000;
+    rotate(secondElement, time);
+    rotate(minuteElement, time / 60);
+    rotate(hourElement, time / 60 / 12);
+    requestAnimationFrame(animate);
+  };
+
+  for (var i = 1 / 4; i <= 60; i += 1 / 4) minute(i);
+  for (var i = 1; i <= 12; i++) hour(i);
+
+  animate();
+}
+
+function autoResize(element, nativeSize) {
+  var update = function () {
+    var parent = element.offsetParent;
+    var scale = Math.min(parent.offsetWidth, parent.offsetHeight) / nativeSize;
+    element.style.transform = element.style.webkitTransform =
+      "scale(" + scale.toFixed(3) + ")";
+  };
+  update();
+  window.addEventListener("resize", update);
+}
+
+const RenderClock = function () {
+  var clock = document.getElementById("utility-clock");
+  utilityClock(clock);
+  autoResize(clock, 295 + 32);
 };
 
 const InitializeCalendar = function (selector) {
-  let calendarDiv = document.createElement("div");
+  let calendarRow = document.createElement("div");
+  calendarRow.classList.add("row");
+  calendarRow.classList.add("border");
+  calendarRow.classList.add("rounded");
+  calendarRow.classList.add("p-3");
+  calendarRow.id = "calendar-outter";
+  selector.appendChild(calendarRow);
+
+  let calendarDiv = document.createElement("span");
+  calendarDiv.classList.add("col");
   calendarDiv.id = "calendar-container";
-  selector.appendChild(calendarDiv);
+  calendarRow.appendChild(calendarDiv);
+
+  let organizerDiv = document.createElement("span");
+  organizerDiv.classList.add("col");
+  organizerDiv.id = "organizer-container";
+  calendarRow.appendChild(organizerDiv);
 
   var calendar = new Calendar(
     "calendar-container",
@@ -323,7 +497,7 @@ const InitializeCalendar = function (selector) {
       indicator: true,
       indicator_type: 1,
       indicator_pos: "top",
-      placeholder: "<span>Custom Placeholder</span>",
+      // placeholder: "<span>Custom Placeholder</span>",
     }
   );
 
@@ -340,6 +514,7 @@ const InitializeCalendar = function (selector) {
       },
     },
   };
+  var organizer = new Organizer("organizer-container", calendar, data);
 };
 
 const InitializeSmartDevicesContainer = function (selector) {
@@ -414,7 +589,7 @@ export async function StartApplication(runTimeData) {
 
               case "start_observe_response":
                 RenderClock();
-                setInterval(RenderClock, 1000);
+                // setInterval(RenderClock, 1000);
 
                 // Render Smart Devices
                 RenderSmartDevices(devicesOnAutomations);
