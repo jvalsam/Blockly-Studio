3c8aae6b713a0bc453e26b8c829d2dedbf83fe19
Linaritis Dimitrios <dimilinaritis@gmail.com>
2020-12-14 15:40:19 +0200

    updating workspace options

--- src/app/ide/components-framework/build-in.components/debugger/blockly-debugger/init_blockly.js
+++ src/app/ide/components-framework/build-in.components/debugger/blockly-debugger/init_blockly.js
@@ -1,40 +1,45 @@
-import './events.js';
+import "./events.js";
 
-import * as Blockly from 'blockly';
-
+import * as Blockly from "blockly";
+import {
+  GetBlocklyWspOptions,
+  Privillege,
+} from "../../../../../ide/ide-components/blockly/blockly-instance.js";
 
 export function InitializeBlocklyDebuggerWorkspace(wsps) {
-    window.workspace = {};
+  window.workspace = {};
 
-    window.workspace["blockly1"] = Blockly.inject(
-        'blocklyDiv',
-        {
-            media: './media/',
-            toolbox: document.getElementById('toolbox')
-        }
-    );
-    window.workspace["blockly1"].systemEditorId = 'blockly1';
-    
-    window.workspace["blockly2"] = Blockly.inject(
-        'blocklyDiv2',
-        {
-            media: './media/',
-            toolbox: document.getElementById('toolbox')
-        }
-    );
-    
-    window.workspace["blockly2"].systemEditorId = 'blockly2';
+  window.workspace["blockly1"] = Blockly.inject(
+    "blocklyDiv",
+    GetBlocklyWspOptions(
+      Privillege.EDITING,
+      "./media/",
+      document.getElementById("toolbox")
+    )
+  );
+  window.workspace["blockly1"].systemEditorId = "blockly1";
 
-    addEventListener("loadStartingBlocks", function () {
-        Blockly.Xml.domToText(document.getElementById('startBlocks'));
-        Blockly.Xml.domToWorkspace(
-            document.getElementById('startBlocks'),
-            window.workspace["blockly1"]
-        );
-    
-        Blockly.Xml.domToWorkspace(
-            document.getElementById('startBlocks'),
-            window.workspace["blockly2"]
-        );
-    });    
+  window.workspace["blockly2"] = Blockly.inject(
+    "blocklyDiv2",
+    GetBlocklyWspOptions(
+      Privillege.EDITING,
+      "./media/",
+      document.getElementById("toolbox")
+    )
+  );
+
+  window.workspace["blockly2"].systemEditorId = "blockly2";
+
+  addEventListener("loadStartingBlocks", function () {
+    Blockly.Xml.domToText(document.getElementById("startBlocks"));
+    Blockly.Xml.domToWorkspace(
+      document.getElementById("startBlocks"),
+      window.workspace["blockly1"]
+    );
+
+    Blockly.Xml.domToWorkspace(
+      document.getElementById("startBlocks"),
+      window.workspace["blockly2"]
+    );
+  });
 }
--- src/app/ide/ide-components/blockly/blockly-wsp-sync.js
+++ src/app/ide/ide-components/blockly/blockly-wsp-sync.js
@@ -1,48 +1,53 @@
+import { GetBlocklyWspOptions, Privillege } from "../blockly/blockly-instance";
 
-function _BlocklyWSPSync () {
-    this.instances = {};
+function _BlocklyWSPSync() {
+  this.instances = {};
 
-    this.isLoaded = (id) => {
-        return id in this.instances;
-    };
+  this.isLoaded = (id) => {
+    return id in this.instances;
+  };
 
-    this.loadWSP = (id, src) => {
-        let blocklySel = "blockly_" + id;
-        $(".blockly-editors-area")
-                .append(
-                    "<div id=\""
-                    + blocklySel
-                    + "\" style=\"position: absolute\"></div>");
-        let blocklyDiv = document.getElementById(blocklySel);
+  this.loadWSP = (id, src) => {
+    let blocklySel = "blockly_" + id;
+    $(".blockly-editors-area").append(
+      '<div id="' + blocklySel + '" style="position: absolute"></div>'
+    );
+    let blocklyDiv = document.getElementById(blocklySel);
 
-        this.instances[id] = Blockly.inject(blocklyDiv, { readOnly: true });
+    this.instances[id] = Blockly.inject(
+      blocklyDiv,
+      GetBlocklyWspOptions(
+        Privillege.READ_ONLY,
+        "../../../../../node_modules/blockly/media/"
+      )
+    );
 
-        if (src) {
-            var xml = Blockly.Xml.textToDom(src);
-            Blockly.Xml.domToWorkspace(xml, this.wsp);
-        }
-    };
-    
-    this.updateWSP = (id, json) => {
-        let event = Blockly.Events.fromJson(json, this.instances[id]);
-        event.run(true);
-    };
-
-    this.getSrc = (id) => {
-        var xml = Blockly.Xml.workspaceToDom(this.instances[id]);
-        return Blockly.Xml.domToText(xml);
-    };
-
-    this.dispose = (id) => {
-        this.instances[id].dispose();
-        delete this.instances[id];
-    };
-
-    this.disposeAll = () => {
-        for(const id in this.instances) {
-            this.dispose(id);
-        }
+    if (src) {
+      var xml = Blockly.Xml.textToDom(src);
+      Blockly.Xml.domToWorkspace(xml, this.wsp);
     }
+  };
+
+  this.updateWSP = (id, json) => {
+    let event = Blockly.Events.fromJson(json, this.instances[id]);
+    event.run(true);
+  };
+
+  this.getSrc = (id) => {
+    var xml = Blockly.Xml.workspaceToDom(this.instances[id]);
+    return Blockly.Xml.domToText(xml);
+  };
+
+  this.dispose = (id) => {
+    this.instances[id].dispose();
+    delete this.instances[id];
+  };
+
+  this.disposeAll = () => {
+    for (const id in this.instances) {
+      this.dispose(id);
+    }
+  };
 }
 
 export const BlocklyWSPSync = new _BlocklyWSPSync();
